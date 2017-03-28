1fc6cc6e62bc77d73d3bd013087a02a770188d99
kidg <294665621@qq.com>
2017-03-28 10:31:06 +0800

    add inspect_shell

--- qb/spiders/indexpage.py
+++ qb/spiders/indexpage.py
@@ -5,7 +5,9 @@ import scrapy
 
 class QiuBaiSpider(scrapy.Spider):
     name ="qb"
-    start_urls = ["http://www.qiushibaike.com/"]
+    start_urls = ["http://www.qiushibaike.com/", ]
 
     def parse(self, response):
+        from scrapy.shell import inspect_response
+        inspect_response(self, response)
         print response.xpath('//div[@class="content"]').extract()
